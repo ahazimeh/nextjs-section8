f97c73b08bc0f7d2dbf3a378300562d83e92524a
ali-pixel38 <ali@pixel38.com>
2022-02-07 22:59:29 +0200

    145

---
+++ pages/feedback/index.js
@@ -0,0 +1,22 @@
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+function FeedbackPageComponent(props) {
+    return (
+        <ul>
+            {props.feedbackItems.map(item =>
+                <li key={item.id}>{item.text}</li>
+            )}
+        </ul>
+    )
+}
+
+export async function getStaticProps() {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+    return {
+        props: {
+            feedbackItems: data
+        }
+    };
+}
+
+export default FeedbackPageComponent;
